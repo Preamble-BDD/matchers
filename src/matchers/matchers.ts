d6ee995b44b92c1a10dcfa7453a50d6932e98369
Jeff Schwartz <jefftschwartz@gmail.com>
2016-03-20 19:36:52 -0400

    Removed deeprecursiveequal.ts from project.

--- src/matchers/matchers.ts
+++ src/matchers/matchers.ts
@@ -2,87 +2,91 @@
  * Preamble's Out Of The Box Matchers
  */
 
-let matchers: IMatcher[] = [];
+(function() {
+    let matchers: IMatcher[] = [];
 
-window["preamble"] = window["preamble"] || {};
-window["preamble"]["matchers"] = window["preamble"]["matchers"] || matchers;
+    window["preamble"] = window["preamble"] || {};
+    window["preamble"]["matchers"] = window["preamble"]["matchers"] || matchers;
 
-// toBeTrue/not.toBeTrue matchers
-matchers.push({
-    apiName: "toBeTrue",
-    api: (): void => { },
-    evalueator: (expectedValue): boolean => expectedValue === true,
-    negator: true,
-    minArgs: 0,
-    maxArgs: 0
-});
+    // toBeTrue/not.toBeTrue matchers
+    matchers.push({
+        apiName: "toBeTrue",
+        api: (): void => { },
+        evalueator: (expectedValue): boolean => expectedValue === true,
+        negator: true,
+        minArgs: 0,
+        maxArgs: 0
+    });
 
-// toBeTruthy/not.toBeTruthy matchers
-matchers.push({
-    apiName: "toBeTruthy",
-    api: (): void => { },
-    evalueator: (expectedValue): boolean => !!expectedValue,
-    negator: true,
-    minArgs: 0,
-    maxArgs: 0
-});
+    // toBeTruthy/not.toBeTruthy matchers
+    matchers.push({
 
-// toBe/not.toBe matchers
-matchers.push({
-    apiName: "toBe",
-    api: (matcherValue: any): any => matcherValue,
-    evalueator: (expectedValue, matcherValue): boolean => expectedValue === matcherValue,
-    negator: true,
-    minArgs: 1,
-    maxArgs: 1
-});
+        
+        apiName: "toBeTruthy",
+        api: (): void => { },
+        evalueator: (expectedValue): boolean => !!expectedValue,
+        negator: true,
+        minArgs: 0,
+        maxArgs: 0
+    });
 
-// toEqual/not.toEqual matchers
-matchers.push({
-    apiName: "toEqual",
-    api: (matcherValue: any): any => matcherValue,
-    evalueator: (expectedValue, matcherValue): boolean => deepRecursiveCompare(expectedValue, matcherValue),
-    negator: true,
-    minArgs: 1,
-    maxArgs: 1
-});
+    // toBe/not.toBe matchers
+    matchers.push({
+        apiName: "toBe",
+        api: (matcherValue: any): any => matcherValue,
+        evalueator: (expectedValue, matcherValue): boolean => expectedValue === matcherValue,
+        negator: true,
+        minArgs: 1,
+        maxArgs: 1
+    });
 
-// toBeDefined/not.toBeDefined matchers
-matchers.push({
-    apiName: "toBeDefined",
-    api: (): void => { },
-    evalueator: (expectedValue): boolean => expectedValue !== undefined,
-    negator: true,
-    minArgs: 0,
-    maxArgs: 0
-});
+    // toEqual/not.toEqual matchers
+    matchers.push({
+        apiName: "toEqual",
+        api: (matcherValue: any): any => matcherValue,
+        evalueator: (expectedValue, matcherValue): boolean => window["preamble"].deepRecursiveCompare(expectedValue, matcherValue),
+        negator: true,
+        minArgs: 1,
+        maxArgs: 1
+    });
 
-// toBeUndefined/not.toBeUndefined matchers
-matchers.push({
-    apiName: "toBeUndefined",
-    api: (): void => { },
-    evalueator: (expectedValue): boolean => expectedValue === undefined,
-    negator: true,
-    minArgs: 0,
-    maxArgs: 0
-});
+    // toBeDefined/not.toBeDefined matchers
+    matchers.push({
+        apiName: "toBeDefined",
+        api: (): void => { },
+        evalueator: (expectedValue): boolean => expectedValue !== undefined,
+        negator: true,
+        minArgs: 0,
+        maxArgs: 0
+    });
 
-// toBeNull/not.toBeNull matchers
-matchers.push({
-    apiName: "toBeNull",
-    api: (): void => { },
-    evalueator: (expectedValue): boolean => expectedValue === null,
-    negator: true,
-    minArgs: 0,
-    maxArgs: 0
-});
+    // toBeUndefined/not.toBeUndefined matchers
+    matchers.push({
+        apiName: "toBeUndefined",
+        api: (): void => { },
+        evalueator: (expectedValue): boolean => expectedValue === undefined,
+        negator: true,
+        minArgs: 0,
+        maxArgs: 0
+    });
 
-// toMatch/not.toMatch matchers
-matchers.push({
-    apiName: "toMatch",
-    api: (matcherValue: RegExp): RegExp => matcherValue ,
-    evalueator: (expectedValue: string, matcherValue: RegExp): boolean => matcherValue.exec(expectedValue) !== null,
-    negator: true,
-    minArgs: 1,
-    maxArgs: 1
-});
+    // toBeNull/not.toBeNull matchers
+    matchers.push({
+        apiName: "toBeNull",
+        api: (): void => { },
+        evalueator: (expectedValue): boolean => expectedValue === null,
+        negator: true,
+        minArgs: 0,
+        maxArgs: 0
+    });
+
+    // toMatch/not.toMatch matchers
+    matchers.push({
+        apiName: "toMatch",
+        api: (matcherValue: RegExp): RegExp => matcherValue,
+        evalueator: (expectedValue: string, matcherValue: RegExp): boolean => matcherValue.exec(expectedValue) !== null,
+        negator: true,
+        minArgs: 1,
+        maxArgs: 1
+    });
+} ());
